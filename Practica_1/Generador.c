1d6cfd102be2d4553c7e76d24b9ce2c68eb24645
Fatake Raicos <pace_lozano@hotmail.com>
2020-01-08 18:15:23 -0600

    Generador update

--- Practica_1/Generador.c
+++ Practica_1/Generador.c
@@ -1,43 +1,83 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <time.h>
+//Constantes
 #define ARGUMENTOS "c:h"
 
+#define MaxPid 97128765
+#define MinPid 1000000
+
+#define MaxTimeEjecu 200
+#define MinTimeEjecu 5
+
+#define MaxPriority 5
+#define MinPriority 1
+
+
 int main (int argc, char *argv[]) {
-	int opcion = 0;
-	char *opcion1Valores = NULL;
+	int opcion, cantidadProcesos = 0, pid, tiempoEspera, prioridad;
+	FILE *fichero;
 
 	while ( (opcion = getopt(argc, argv, ARGUMENTOS)) != -1) {
 		switch( opcion ) {
-			case 'c':
-				/*
-				 optarg tiene los valores despues del argumento
-				 -algo valores
-				 optarg = valores;
-				 si no existen valores
-				 optarg = (NULL);
-				 */
-				opcion1Valores = optarg;
-				printf("\nOpcion i = %s", opcion1Valores);
+			case 'c'://Cantidad procesos
+				cantidadProcesos = atoi(optarg);
 			break;
-			case 'h':
-				printf("");
-				printf("Parametos admitidos\n -c valor \n");
-				printf("-o\n -h ayuda\n");
-				return 0;
+
+			case 'h'://Ayuda
+				printf("Programa generador de procesos en un Archivo\n");
+				printf("Uso:\n-c <Int:cantidad>  [Indica al programa cuandos procesos se crearan] \n");
+				printf("-h [ayuda]\n");
+				printf("\n Ejemplo: ./Generador -c 200");
+				exit(EXIT_SUCCESS);
 			break;
-			case '?':
-				/*
-				 getopt retorna ? para parametros desconocidos
-				 */
-				if (optopt == 'i') {
-					printf("\nMissing mandatory input option");
+
+			case '?'://Argumento no reconocido o falta de argumentos
+				if (optopt == 'c') {
+					printf("\nFanta indicar cantidad de procesos\n");
 				} else {
-					printf("\nParametro desconocido");
+					printf("\nArgumento desconocido\n");
 				}
+				printf("Use el argumento -h para tener informacion\n");
+				exit(EXIT_FAILURE);
 			break;
 		}
 	}
+	/*
+	 * Inicio de proceso
+	 */
+	if (cantidadProcesos ==0 ){
+		printf("Error en la lectura del parametro -c\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	//Apertura de Archivo
+	if ((fichero = fopen("ProcesosGenerados.txt", "w")) == NULL){
+		printf("Error al abrir el archivo\n");
+		exit(EXIT_FAILURE);
+	}
+	srand ( time(NULL) + 52 );
 
+	for (int i = 0; i < cantidadProcesos; i++){
+		pid          = rand () % (MaxPid - MinPid + 1) + MinPid;
+		tiempoEspera = rand () % (MaxTimeEjecu - MinTimeEjecu + 1) + MinTimeEjecu;
+		prioridad    = rand () % (MaxPriority - MinPriority + 1) + MinPriority;
+
+		fprintf(fichero, "%d", pid);
+		fprintf(fichero, " ");
+		fprintf(fichero, "%d", tiempoEspera);
+		fprintf(fichero, " ");
+		fprintf(fichero, "%d", prioridad);
+		fprintf(fichero, "\n");
+	}
+	
+	if (fclose( fichero ) != 0){
+    	printf("Error al cerrar el fichero\n");
+		exit(EXIT_FAILURE);
+  	}
+	printf("Cantidad de datos introducidos %d\n",cantidadProcesos);
 	printf("\n");
 	return 0;
  }
